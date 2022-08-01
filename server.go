1f30a70e78500eaad8ae321db92f282dcf121b65
interesting1113 <sylviayang1113@gmail.com>
2022-08-01 18:58:20 +0900

    first commit

---
+++ server.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"sync"
+)
+
+type Server struct {
+	Ip string
+	Port int
+
+	// 在线用户列表
+	OnlineMap map[string]*User
+	mapLock sync.RWMutex
+
+	// 消息广播的channel
+	Message chan string
+}
+
+// 监听Message广播消息的goroutine, 一旦有消息就发送给全部在线的user
+func (this *Server) ListenMessage()  {
+	for {
+		msg := <- this.Message
+
+		// 将消息发送给全部在线的user
+		this.mapLock.Lock()
+		for _, cli := range this.OnlineMap {
+			cli.C <- msg
+		}
+		this.mapLock.Unlock()
+	}
+}
+
+// 创建一个server的接口
+func NewServer(ip string, port int) *Server {
+	server := &Server{
+		Ip: ip,
+		Port: port,
+		OnlineMap: make(map[string]*User),
+		Message: make(chan string),
+	}
+
+	return server
+}
+
+// 广播消息的方法
+func (this *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	this.Message <- sendMsg
+}
+
+func (this *Server) Handler(conn net.Conn)  {
+	// ...当前链接业务
+	fmt.Println("链接建立成功")
+	user := NewUser(conn)
+
+	// 用户上线， 将用户加入到OnlineMap中
+	this.mapLock.Lock()
+	this.OnlineMap[user.Name] = user
+	this.mapLock.Unlock()
+
+	// 广播当前用户上线消息
+	this.BroadCast(user, "已上线")
+
+	// 接收客户端发送的消息
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				this.BroadCast(user, "下线")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err: ", err)
+				return
+			}
+
+			msg := string(buf[:n - 1])
+
+			// 将得到的消息进行广播
+			this.BroadCast(user, msg)
+		}
+	}()
+	// 当前handler阻塞
+	select {
+
+	}
+}
+
+func (this *Server) Start()  {
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", this.Ip, this.Port))
+	if err != nil {
+		fmt.Printf("new.Listen error:", err)
+		return
+	}
+	// close listen socket
+	defer listener.Close()
+
+	// 启动监听Message的goroutine
+	go this.ListenMessage()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error", err)
+			continue
+		}
+
+		// do handler
+		go this.Handler(conn)
+	}
+}
